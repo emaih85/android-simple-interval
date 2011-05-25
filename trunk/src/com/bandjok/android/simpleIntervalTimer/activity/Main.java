d4ec1ff00f8eff3cec9865aa02ea32d3aa16ff27
ben.s.okeeffe@gmail.com <ben.s.okeeffe@gmail.com@45c5771c-f327-7738-a6ea-a4b6672871e6>
2011-05-25 12:15:22 +0000

    Useless commit..

---
+++ trunk/src/com/bandjok/android/simpleIntervalTimer/activity/Main.java
@@ -0,0 +1,12 @@
+package com.bandjok.android.simpleIntervalTimer.activity;
+
+import android.app.Activity;
+
+/**
+ * Main activity for the Simple Interval Timer
+ * @author Ben
+ *
+ */
+public class Main extends Activity {
+
+}
